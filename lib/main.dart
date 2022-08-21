db81baa1342dd849a80544d22a8089e125685f54
omarageorge <georgeomara8@gmail.com>
2022-08-22 00:39:12 +0300

    Organised app routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:belendroit/components/bottom_nav.dart';
 import 'package:belendroit/screens/home_screen.dart';
+import 'package:belendroit/screens/liked_screen.dart';
+import 'package:belendroit/screens/notifications_screen.dart';
 
-void main() {
-  runApp(const Main());
+void main() => runApp(const MainScreen());
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> pages = [
+    const HomeScreen(),
+    const NotificationsScreen(),
+    const LikedScreen(),
+  ];
+
+  void onTap(int? index) {
+    setState(() {
+      _currentIndex = index!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Belendroid',
-      home: Scaffold(
-        body: HomeScreen(),
+      theme: ThemeData(
+        inputDecorationTheme:
+            const InputDecorationTheme(prefixIconColor: Colors.black54),
       ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(child: pages[_currentIndex]),
+        bottomNavigationBar:
+            BottomNavBar(currentIndex: _currentIndex, onTap: onTap),
+      ),
+      routes: {
+        HomeScreen.pageId: (context) => const HomeScreen(),
+        NotificationsScreen.pageId: (context) => const NotificationsScreen(),
+        LikedScreen.pageId: (context) => const LikedScreen(),
+      },
     );
   }
 }
