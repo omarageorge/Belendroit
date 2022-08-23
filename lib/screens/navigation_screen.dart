d2c16658e6f9e06852690a040486acd6c3326c21
omarageorge <georgeomara8@gmail.com>
2022-08-23 10:38:45 +0300

    Manages navigation from screen to screen

---
+++ lib/screens/navigation_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:belendroit/components/bottom_nav.dart';
+import 'package:belendroit/screens/home_screen.dart';
+import 'package:belendroit/screens/bookmark_screen.dart';
+import 'package:belendroit/screens/notifications_screen.dart';
+import 'package:belendroit/screens/account_screen.dart';
+
+class NavigationScreen extends StatefulWidget {
+  const NavigationScreen({Key? key}) : super(key: key);
+
+  static String id = 'navigation_screen';
+
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> pages = [
+    const HomeScreen(),
+    const NotificationsScreen(),
+    const BookmarkScreen(),
+    const AccountScreen()
+  ];
+
+  void onTap(int? index) {
+    setState(() {
+      _currentIndex = index!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(child: pages[_currentIndex]),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: onTap,
+      ),
+    );
+  }
+}
